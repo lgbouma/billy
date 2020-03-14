6cb17cbbd9f0314541a35eea680f770a6987d443
lgbouma <bouma.luke@gmail.com>
2020-03-14 17:00:26 -0400

    and multi-type list flatten convenience function

--- billy/convenience.py
+++ billy/convenience.py
@@ -1,3 +1,6 @@
+import numpy as np, pandas as pd
+import collections
+
 def chisq(y_mod, y_obs, y_err):
     return np.sum( (y_mod - y_obs )**2 / y_err**2 )
 
@@ -8,4 +11,12 @@ def bic(chisq, k, n):
     """
     return chisq + k*np.log(n)
 
-
+def flatten(l):
+    for el in l:
+        if (
+            isinstance(el, collections.Iterable) and
+            not isinstance(el, (str, bytes))
+        ):
+            yield from flatten(el)
+        else:
+            yield el
