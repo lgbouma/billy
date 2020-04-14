8e6c73490e381caf379f33e94f76f1fca4e48bc9
lgbouma <bouma.luke@gmail.com>
2020-04-14 14:07:19 -0700

    paper: many updates

--- paper/collect.sh
+++ paper/collect.sh
@@ -1,15 +1,15 @@
 # Usage: copy in figures for the paper
 
-pdir=../results/PTFO_8-8695_results/20200320_linear_amplitude_prior/
+pdir=../results/PTFO_8-8695_results/20200413_v0/
 
-#cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_splitsignalmap_joined.pdf f1.pdf
-cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_splitsignalmap.png f1.png
+cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_splitsignalmap_i.pdf f1.pdf
+cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_splitsignalmap_ii.pdf f2.pdf
 
-cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_phasefoldmap.pdf f2.pdf
+cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_phasefoldmap.pdf f3.pdf
 
-cp ../results/PTFO_8-8695_results/scene.pdf f3.pdf
+cp ../results/PTFO_8-8695_results/scene.pdf f4.pdf
 
-cp ../results/ephemeris/O_minus_C.pdf f4.pdf
+cp ../results/ephemeris/O_minus_C.pdf f5.pdf
 
-cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_cornerplot.png f5.png
-pngquant f5.png --output f5_comp.png
+cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_cornerplot.png f6.png
+pngquant f6.png --output f6_comp.png
