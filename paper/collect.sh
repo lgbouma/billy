c215c1ecf5fc4123a7bb874bc946474077a326b7
lgbouma <bouma.luke@gmail.com>
2020-04-17 17:23:02 -0700

    update paper

--- paper/collect.sh
+++ paper/collect.sh
@@ -9,9 +9,11 @@ cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_phasefoldmap.pdf f3.pdf
 
 cp ../results/PTFO_8-8695_results/scene.pdf f4.pdf
 
-cp ../results/ephemeris/O_minus_C.pdf f5.pdf
+cp ../results/cluster_membership/hr.pdf f5.pdf
 
-cp ../results/cluster_membership/hr.pdf f6.pdf
+cp ../results/ephemeris/O_minus_C.pdf f6.pdf
+
+cp ../results/brethren/brethren.pdf f7.pdf
 
 # cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_cornerplot.png f6.png
 # pngquant f6.png --output f6_comp.png
