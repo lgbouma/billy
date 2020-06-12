45f63e9f249c86188e355fb5d76df79c47461b46
lgbouma <bouma.luke@gmail.com>
2020-06-11 17:51:12 -0700

    WIP for the referee report
    added some nice details to flesh out the torus of gas+dust in favor of
    the starspot occultations

--- paper/collect.sh
+++ paper/collect.sh
@@ -10,7 +10,7 @@ cp ${pdir}PTFO_8-8695_transit_3sincosPorb_2sincosProt_phasefoldmap.pdf f3.pdf
 cp ../results/PTFO_8-8695_results/scene.pdf f4.pdf
 
 cp ../results/cluster_membership/hr.pdf f5a.pdf
-cp ../results/cluster_membership/astrometric_excess.pdf f5b.pdf
+cp ../results/cluster_membership/astrometric_excess_ruwe_talklabels.pdf f5b.pdf
 
 cp ../results/ephemeris/O_minus_C.pdf f6.pdf
 
